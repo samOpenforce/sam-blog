6e244139453cc1b0276f1fdbd99507f1a7f4a903
Sam Scholefield <sam.scholefield@openforce.com>
2020-08-15 18:27:02 +0200

    runs develop once; text changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,8 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>tasaal</h1>
+    <p>A blog about my experiences in the Frontend</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -7,9 +7,9 @@ import SEO from "../components/seo"
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <h1>PAGE TEMPLATE HEADING</h1>
+    <p>Page template paragraph</p>
+    <Link to="/">return to index</Link>
   </Layout>
 )
 
